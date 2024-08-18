f601033b3f2f564fab7bcd0c25405c5d53d9bc22
Agustin Augurusa <augurusaagustin@gmail.com>
2024-08-18 15:53:32 -0300

    Adds a delete user page where admin can delete users by their user code

---
+++ src/pages/deleteUser/index.tsx
@@ -0,0 +1,76 @@
+import React, {useState} from "react";
+import "./styles.css";
+import { Box } from "@mui/material";
+import { RightArrowIcon } from "../../assets/Icons/RightArrowIcon";
+import { LeftArrowIcon } from "../../assets/Icons/LeftArrowIcon";
+import {userMock} from "../../mocks";
+import EditableInput from "../../components/EditableInput";
+import Button from "../../components/Button";
+import styles from "../activity/styles.module.css";
+
+type attributeType = keyof typeof userMock;
+
+const DeleteUser = () => {
+  const options = [
+    {
+      name: "Usuarios",
+    },
+    {
+      name: "Tratamientos",
+    },
+  ];
+
+  const [mockUser, setMockUser] = useState(userMock);
+
+
+  const [route, setRoute] = React.useState("");
+
+  //orden: tratamientos, modulo, actividades
+
+  const handleClick = (option: string) => {
+    setRoute(option);
+  };
+
+  const handleChange = (attribute:attributeType, newValue:string) => {
+    setMockUser({...mockUser, [attribute]: newValue })
+  }
+
+  const handleBackClick = () => {
+    setRoute("");
+  };
+
+  const handleSubmit = () => {
+    console.log("Deleted user: ", mockUser)
+  }
+
+  return (
+    <Box className={"home-display"}>
+      <Box className={"home-menu"}>
+        {options.map((option, index) => (
+          <Box
+            key={index}
+            className={"menu-option"}
+            onClick={() => handleClick(option.name)}
+          >
+            <h5>{option.name}</h5>
+            <RightArrowIcon width="16" height="16" />
+          </Box>
+        ))}
+      </Box>
+      <Box className={"module"}>
+        <Box className={"route"}>
+          <LeftArrowIcon width="16" height="16" onClick={handleBackClick} />
+          <h6>{route}</h6>
+        </Box>
+
+        <Box className={styles.activityContainer}>
+          <EditableInput title={'Codigo de usuario:'} text={mockUser.code} placeholder={'afeaf'} type={'text'} name={'UserCode'} handleChange={(e) => handleChange('code', e.target.value)}/>
+          <Button onClick={() => handleSubmit()} variant={'primary'} size={'medium'}>Guardar</Button>
+        </Box>
+
+      </Box>
+    </Box>
+  );
+};
+
+export default DeleteUser;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,76 +1,56 @@
-import React, {useState} from "react";
+import React from "react";
 import "./styles.css";
-import { Box } from "@mui/material";
-import { RightArrowIcon } from "../../assets/Icons/RightArrowIcon";
-import { LeftArrowIcon } from "../../assets/Icons/LeftArrowIcon";
-import {userMock} from "../../mocks";
-import EditableInput from "../../components/EditableInput";
-import Button from "../../components/Button";
-import styles from "../activity/styles.module.css";
+import {Box} from "@mui/material";
+import {RightArrowIcon} from "../../assets/Icons/RightArrowIcon";
+import {LeftArrowIcon} from "../../assets/Icons/LeftArrowIcon";
 
-type attributeType = keyof typeof userMock;
 
 const Home = () => {
-  const options = [
-    {
-      name: "Usuarios",
-    },
-    {
-      name: "Tratamientos",
-    },
-  ];
+    const options = [
+        {
+            name: "Usuarios",
+        },
+        {
+            name: "Tratamientos",
+        },
+    ];
 
-  const [mockUser, setMockUser] = useState(userMock);
+    const [route, setRoute] = React.useState("");
+
+    //orden: tratamientos, modulo, actividades
+
+    const handleClick = (option: string) => {
+        setRoute(option);
+    };
 
 
-  const [route, setRoute] = React.useState("");
+    const handleBackClick = () => {
+        setRoute("");
+    };
 
-  //orden: tratamientos, modulo, actividades
 
-  const handleClick = (option: string) => {
-    setRoute(option);
-  };
-
-  const handleChange = (attribute:attributeType, newValue:string) => {
-    setMockUser({...mockUser, [attribute]: newValue })
-  }
-
-  const handleBackClick = () => {
-    setRoute("");
-  };
-
-  const handleSubmit = () => {
-    console.log("Deleted user: ", mockUser)
-  }
-
-  return (
-    <Box className={"home-display"}>
-      <Box className={"home-menu"}>
-        {options.map((option, index) => (
-          <Box
-            key={index}
-            className={"menu-option"}
-            onClick={() => handleClick(option.name)}
-          >
-            <h5>{option.name}</h5>
-            <RightArrowIcon width="16" height="16" />
-          </Box>
-        ))}
-      </Box>
-      <Box className={"module"}>
-        <Box className={"route"}>
-          <LeftArrowIcon width="16" height="16" onClick={handleBackClick} />
-          <h6>{route}</h6>
+    return (
+        <Box className={"home-display"}>
+            <Box className={"home-menu"}>
+                {options.map((option, index) => (
+                    <Box
+                        key={index}
+                        className={"menu-option"}
+                        onClick={() => handleClick(option.name)}
+                    >
+                        <h5>{option.name}</h5>
+                        <RightArrowIcon width="16" height="16"/>
+                    </Box>
+                ))}
+            </Box>
+            <Box className={"module"}>
+                <Box className={"route"}>
+                    <LeftArrowIcon width="16" height="16" onClick={handleBackClick}/>
+                    <h6>{route}</h6>
+                </Box>
+            </Box>
         </Box>
-
-        <Box className={styles.activityContainer}>
-          <EditableInput title={'Codigo de usuario:'} text={mockUser.code} placeholder={'afeaf'} type={'text'} name={'UserCode'} handleChange={(e) => handleChange('code', e.target.value)}/>
-          <Button onClick={() => handleSubmit()} variant={'primary'} size={'medium'}>Guardar</Button>
-        </Box>
-
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default Home;
