f9aeee3004138fcf366d9e523a2800e750805e2d
maxigeist <geist.maximo@gmail.com>
2024-08-23 16:33:54 -0300

    feat: add functionality to the stepper using redux, the option to navigate to the previous visited page is now available

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -3,6 +3,9 @@ import {RightArrowIcon} from "../../assets/Icons/RightArrowIcon";
 import {useState} from "react";
 import styles from './styles.module.css'
 import React from "react";
+import {useAppDispatch, useAppSelector} from "../../redux/hooks.ts";
+import {Route, updateRoutePath} from "../../redux/routeSlice.ts";
+import {useNavigate} from "react-router-dom";
 
 export type OptionsType = {
     name: string;
@@ -18,7 +21,7 @@ export const SideBar = () => {
         {
             name: "Usuarios",
             isOpen: false,
-            redirect: '/'
+            redirect: '/users/'
         },
         {
             name: "Tratamientos",
@@ -39,15 +42,19 @@ export const SideBar = () => {
     ];
 
     const [options, setOptions] = useState(optionsMock);
+    const dispatch = useAppDispatch();
+    const route: Route = useAppSelector((store) => store.route)
+    const navigate = useNavigate()
 
     const handleSelectItem = (index: number) => {
         const updatedOptions = options.map((option, i) =>
             i === index ? {...option, isOpen: !option.isOpen} : option
         );
         // falta el set de la route cuando se hace un select
-
+        const optionSelected = options[index]
+        dispatch(updateRoutePath({id:'', name:optionSelected.name, route:optionSelected.redirect, position: route.path.length}))
+        navigate(optionSelected.redirect ? optionSelected.redirect : '')
         setOptions(updatedOptions);
-
     }
 
     return (
