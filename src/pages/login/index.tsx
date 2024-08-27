b310596509726495c1a291e7b6951af25e52d4c3
Agustin Augurusa <augurusaagustin@gmail.com>
2024-08-27 15:29:43 -0300

    Creates a LogIn page for admin missing bigger inputs and deleting side bar

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './styles.css';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -6,11 +6,12 @@ import InputField from "../../components/InputField";
 import Button from "../../components/Button";
 import { emptyAdminMock } from "../../mocks";
 import { Form } from "react-router-dom";
+import logo from '../../assets/Logo/logo.png';
 
 type attributeType = keyof typeof emptyAdminMock;
 
 const LoginPage = () => {
-    const [adminData, setAdminData] = useState({ adminEmail: '', adminPassword: '' });
+    const [adminData, setAdminData] = useState({ email: '', password: '' });
 
     const handleChange = (attribute: attributeType, newValue: string) => {
         setAdminData({ ...adminData, [attribute]: newValue });
@@ -23,21 +24,24 @@ const LoginPage = () => {
     return (
         <Box className="loginPageContainer">
             <Box className="headerContainer">
+                <img src={logo} alt="logo" width={100} height={150}/>
                 <Typography variant="h3" className="title">Renacentia</Typography>
                 <Typography variant="subtitle1" className="subtitle">Administración</Typography>
             </Box>
             <Form className="inputContainer">
                 <InputField
-                    text={adminData.adminEmail}
+                    text={adminData.email}
                     placeholder="Ingrese su email"
                     name="adminEmail"
                     handleChange={(e) => handleChange('email', e.target.value)}
                 />
-                <InputField
-                    text={adminData.adminPassword}
+                <input
+                    className={"input"}
+                    value={adminData.password}
+                    type="password"
                     placeholder="Ingrese su contraseña"
                     name="adminPassword"
-                    handleChange={(e) => handleChange('password', e.target.value)}
+                    onChange={(e) => handleChange('password', e.target.value)}
                 />
                 <Box className="buttonContainer">
                     <Button onClick={handleSubmit} variant="green">
