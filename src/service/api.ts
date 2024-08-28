edf750cff0ae2a136eab6ca5173c33aa4625d8cf
Agustin Augurusa <augurusaagustin@gmail.com>
2024-08-28 12:15:19 -0300

    WIP

--- src/service/api.ts
+++ src/service/api.ts
@@ -73,6 +73,11 @@ export const createUser = async (data: any) => {
   return response.data;
 };
 
+export const deleteUser = async (patient_code: string) => {
+  const response = await api.delete(`admin/user/delete/${patient_code}`);
+  return response.data;
+};
+
 export const useUpdateActivity = () => {
   const queryClient = useQueryClient();
   return useMutation({
