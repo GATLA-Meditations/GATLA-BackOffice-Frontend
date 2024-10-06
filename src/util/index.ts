7a3651d5a193800e3a1dc119cd611025e2023e94
maxigeist <geist.maximo@gmail.com>
2024-10-06 15:46:40 -0300

    feat: add integration with the backend to upload a content for the item shop

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,8 @@
 export const generateRandomId = () => {
   return Math.random().toString(36).substring(2, 15);
 };
+
+export const parseShopItemsName: { [key: string]: string }  = {
+  'FONDO': 'BACKGROUND',
+  'ICONO': 'AVATAR'
+}
