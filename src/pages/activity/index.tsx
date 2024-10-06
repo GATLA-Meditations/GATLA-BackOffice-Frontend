9bcd9383cc89f64c3c72aec25020978c60ed3287
santiagoperes <santiago.peres@ing.austral.edu.ar>
2024-10-06 19:03:02 -0300

    update activity

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,50 +1,93 @@
 import EditableInput from "../../components/EditableInput";
 import {Box} from "@mui/material";
-import {activityMock} from "../../mocks";
-import {useState} from "react";
+import { useEffect, useState } from 'react';
 import styles from './styles.module.css'
 import Button from "../../components/Button";
 import {useParams} from "react-router-dom";
-import {useGetActivity} from "../../service/api.ts";
-
-
-type attributeType = keyof typeof activityMock;
+import { useGetActivity, useUpdateActivity } from '../../service/api.ts';
+import { Activity, ActivityContent } from '../../types';
 
 const ActivityEdit = () => {
 
     const activityId = useParams().id;
-    const [mockActivity, setMockActivity] = useState(activityMock);
     const {data, isLoading} = useGetActivity(activityId as string);
+    const [activity, setActivity] = useState<Activity>();
+    const [contents, setContents] = useState<ActivityContent[]>([]);
+    const {mutate: updateActivity} = useUpdateActivity();
 
-    const handleChange = (attribute:attributeType, newValue:string) => {
-        setMockActivity({...mockActivity, [attribute]: newValue })
+    useEffect(() => {
+        if(data){
+            setActivity(data);
+            setContents(data.contents)
+        }
+    }, [data]);
+
+    const handleChange = (key: string, value: string, contentId?: string) => {
+        if(activity){
+            if(contentId){
+                const newContents = contents.map((content) => {
+                    if(content.id === contentId){
+                        return {...content, [key]: value}
+                    }
+                    return content
+                })
+                setContents(newContents)
+                setActivity({...activity, contents: newContents})
+            } else
+            setActivity({...activity, [key]: value})
+        }
+    }
+
+    const getContentTitle = (type: string) => {
+        switch (type) {
+            case 'TEXT':
+                return 'Texto';
+            case 'VIDEO':
+                return 'Video';
+            default:
+                return '';
+        }
+    }
+
+    const getContentPlaceholder = (type: string) => {
+        switch (type) {
+            case 'TEXT':
+                return 'Contenido';
+            case 'VIDEO':
+                return 'URL del video';
+            default:
+                return '';
+        }
     }
 
     const handleSubmit = () => {
-        // First would be the post to the backend then the dispatch
-        console.log(mockActivity)
+        if(activity){
+            updateActivity({content: contents, activity: {id: activity.id, title: activity.name}})
+        }
     }
 
     if(isLoading){
         return <h1>Is loading</h1>
     }
 
-    if (data) {
+    if (activity && contents) {
         return (
             <Box className={styles.activityContainer}>
-                <EditableInput title={'Título'} text={data.name} placeholder={'afeaf'} type={'text'} name={'Título'}
-                               handleChange={(e) => handleChange('title', e.target.value)}/>
-                <EditableInput title={'Descripción'} text={data.contents[0].content} placeholder={'anfeanef'}
-                               type={'text'} name={'Descripción'}
-                               handleChange={(e) => handleChange('description', e.target.value)}/>
-                <EditableInput title={'Url del video'} text={data.contents[1].content} placeholder={'aeffafea'}
-                               type={'text'} name={'Url del video'}
-                               handleChange={(e) => handleChange('videoUrl', e.target.value)}/>
+                <EditableInput title={'Título'} text={activity.name} placeholder={'Título'} type={'text'} name={'title'}
+                               handleChange={(e) => handleChange('name', e.target.value)}/>
+                {contents.map((content) => (
+                    <EditableInput
+                        key={content.id}
+                        text={content.content}
+                        placeholder={getContentPlaceholder(content.type)}
+                        type={'text'}
+                        name={content.type}
+                        title={getContentTitle(content.type)}
+                        handleChange={(e) => handleChange('content', e.target.value, content.id)} />
+                ))}
                 <Button onClick={() => handleSubmit()} variant={'primary'} size={'medium'}>Guardar</Button>
             </Box>
         )
     }
-
-
 }
 export default ActivityEdit
