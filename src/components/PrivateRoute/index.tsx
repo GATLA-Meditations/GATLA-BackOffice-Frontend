38c642cd3a371e4144bdfd87e6a32b3848375608
maxigeist <geist.maximo@gmail.com>
2024-11-01 16:56:36 -0300

    feat: add redirection to the login of the backoffice when the token is invalid

---
+++ src/components/PrivateRoute/index.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from "react-router-dom";
+import {useGetUsers} from "../../service/api.ts";
+import {getToken} from "../../service/store.ts";
+
+const PrivateRoute = () => {
+    const token = getToken();
+    // Make a query to the backend to verify that the token is valid
+    useGetUsers(1);
+    return token ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
